99018bf4f8d94563d29b9e7ec35433d50c9eaac8
David Valdepenas Monhco <gumbautorrat@hotmail.com>
2016-03-08 22:46:48 +0100

    Subo el cliente rest

---
+++ app/libs/connect.php
@@ -0,0 +1,46 @@
+<?php if(!defined("SPECIALCONSTANT")) die("Acceso denegado");
+
+function getConnection()
+{
+
+	try{
+
+		$db_username = "root";
+		$db_password = "";
+		$connection = new PDO("mysql:host=localhost;dbname=restfulslim", $db_username, $db_password);
+		$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		$connection->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+	}
+	catch(PDOException $e)
+	{
+		echo "Error: " . $e->getMessage();
+	}
+	return $connection;
+	
+}
+
+function getConnectionAgencia()
+{
+
+	try{
+
+		$db_username = "agencia";
+		$db_password = "123456";
+
+		$host = "localhost";
+		$db_name = "federacion";
+		$db_conection_string = "mysql:host=".$host.";dbname=".$db_name;
+
+		$connection = new PDO($db_conection_string,$db_username, $db_password,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
+		$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		$connection->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+	}
+	catch(PDOException $e)
+	{
+		echo "Error: " . $e->getMessage();
+	}
+	return $connection;
+
+}
---
+++ app/routes/api.php
@@ -0,0 +1,160 @@
+<?php
+
+if(!defined("SPECIALCONSTANT")) die("Acceso denegado");
+
+/*************************************************************************************
+
+	Funcion que se recibe peticiones POST a la url http://localhost/apirest/control
+	y retorna un mensaje informandonos de si la autenticacion ha tenido exito.
+
+
+**************************************************************************************/
+
+$app->post("/control/", function() use($app)
+{
+
+	$pass = $app->request->post("pass");
+	$user = $app->request->post("user");
+	
+	try{
+
+		$connection = getConnectionAgencia();
+		$query = "SELECT COUNT(*) FROM authagencia WHERE pass = ? and id_agencia = (select id_agencia FROM agencias WHERE nombre = ?)";
+		$dbh = $connection->prepare($query);
+		$dbh->bindParam(1, $pass);
+		$dbh->bindParam(2, $user);
+		$dbh->execute();
+
+		$app->response->headers->set("Content-type", "application/json");
+		$app->response->status(200);
+
+		if($dbh->fetchColumn() == 0){
+			$connection = null;
+			$app->response->body(json_encode(array("status" => "Ok", "message" => "Autentication Failure")));
+		}else{
+			$connection = null;
+			$app->response->body(json_encode(array("status" => "Ok", "message" => "Autentication Succesful")));
+		}
+
+	}
+	catch(PDOException $e)
+	{
+		echo "Error: " . $e->getMessage();
+	}
+	
+});
+
+/*************************************************************************************
+
+	Funcion que se recibe peticiones GET a la url http://localhost/apirest/inmuebles
+	y retorna todos los inmuebles en formato JSON
+
+
+**************************************************************************************/
+
+$app->get("/inmuebles/", function() use($app)
+{
+
+	try{
+		$connection = getConnectionAgencia();
+		$dbh = $connection->prepare("SELECT * FROM inmuebles");
+		$dbh->execute();
+		$inmuebles = $dbh->fetchAll();
+		$connection = null;
+
+		$app->response->headers->set("Content-type", "application/json");
+		$app->response->status(200);
+		$app->response->body(json_encode($inmuebles));
+	}
+	catch(PDOException $e)
+	{
+		echo "Error: " . $e->getMessage();
+	}
+
+});
+
+/*************************************************************************************
+
+	Funcion que se recibe peticiones POST a la url http://localhost/apirest/inmuebles
+	y retorna todos los inmuebles tanto propios com compartidos en formato JSON
+
+
+**************************************************************************************/
+
+$app->post("/inmuebles/", function() use($app)
+{
+
+	$agencia = $app->request->post("agencia");
+
+	try{
+
+		$connection = getConnectionAgencia();
+		$dbh = $connection->prepare("SELECT COUNT(*) FROM agencias where nombre = ?");
+		$dbh->bindParam(1, $agencia);
+		$dbh->execute();
+
+		if($dbh->fetchColumn() == 0){ //Si la agencia no existe le mandamos un error
+
+			$connection = null;
+			$app->response->headers->set("Content-type", "application/json");
+			$app->response->status(200);
+			$app->response->body(json_encode(array("result" => "ERROR")));
+
+		}else{//Si la agencia existe
+
+			$consulta = "SELECT * FROM inmuebles where id_agencia = (SELECT id_agencia FROM agencias where nombre = ?) or compartir = 1";
+			$dbh = $connection->prepare($consulta);
+			$dbh->bindParam(1, $agencia);
+			$dbh->execute();
+			$inmuebles = $dbh->fetchAll();
+			$connection = null;
+
+			$app->response->headers->set("Content-type", "application/json");
+			$app->response->status(200);
+			$app->response->body(json_encode($inmuebles));
+
+		}
+		
+	}catch(PDOException $e){
+		echo "Error: " . $e->getMessage();
+	}
+
+});
+
+/***********************************************************************************************
+
+	Funcion que se recibe peticiones POST a la url http://localhost/apirest/propietarios_agencia
+	y devuelve los propietarios de una agencia en formato JSON
+
+
+************************************************************************************************/
+
+$app->get("/propietarios_agencia/:agencia", function($agencia) use($app)
+{
+
+	try{
+
+		$connection = getConnectionAgencia();
+
+		$query = "SELECT distinct(p.dni), p.id_propietario,p.nombre, p.primer_apellido, p.segundo_apellido, p.direccion, p.localidad, p.provincia, p.telefono, p.email
+			      FROM inmuebles i INNER JOIN propietarios p ON i.id_propietario = p.id_propietario 
+		          WHERE i.id_agencia = (SELECT id_agencia FROM agencias WHERE nombre = ?) 
+			      ORDER BY p.nombre,p.primer_apellido,p.segundo_apellido";
+		
+		$dbh = $connection->prepare($query);
+		$dbh->bindParam(1, $agencia);
+		$dbh->execute();
+		$propietarios = $dbh->fetchAll();
+		$connection = null;
+
+		$app->response->headers->set("Content-type", "application/json");
+		$app->response->status(200);
+		$app->response->body(json_encode($propietarios));
+		
+	}
+	catch(PDOException $e)
+	{
+		echo "Error: " . $e->getMessage();
+	}
+
+});
